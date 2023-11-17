3164d9fd71554bb47bbb130b7d360e59c75f33ac
chlorella71 <chlorella71@naver.com>
2023-11-17 12:27:14 +0900

    .

---
+++ work/Helloo.java
@@ -0,0 +1,8 @@
+/**
+* Hello
+*/
+public class Helloo {
+  public static void main(String[] args) {
+    System.out.println("Hello, world!");
+  }
+}
---
+++ work/Kingsman.java
@@ -0,0 +1,2 @@
+public class Kingsman{public static void main(String[] args){System.out.println("manner makth men");}
+}
